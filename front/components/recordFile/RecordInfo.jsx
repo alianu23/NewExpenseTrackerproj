1f46c11bd42c67369b863cdb81e2817f7148e5f1
alianu23 <142651654+alianu23@users.noreply.github.com>
2023-11-18 15:36:21 +0800

    records undsen nuur duusaw

--- front/components/recordFile/RecordInfo.jsx
+++ front/components/recordFile/RecordInfo.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import RecordSmall from "./recordLeft/RecordsCategory/recordTop/RecordSmall";
+import RecordSmall from "./recordLeft/recordTop/RecordSmall";
 import RecordBig from "./recordLeft/RecordsCategory/RecordBig";
-import Range from "./recordLeft/RecordsCategory/recordBottom/Range";
+import Range from "./recordLeft/recordBottom/Range";
+import RightPagiTop from "./recordRight/RightPagi";
+import BottomRecord from "./recordRight/BottomPart/BottomRecord";
 
 const RecordInfo = () => {
   return (
-    <div className="flex mb-60 container py-6 mx-auto">
+    <div className="flex container py-6 mx-auto">
       <div className="flex-0.5 border-2 bg-[#F9FAFB] px-5 py-8 rounded-xl">
         <RecordSmall />
         <RecordBig />
         <Range />
       </div>
-      <div className="flex-1"></div>
+      <div className="flex-1 px-4">
+        <RightPagiTop />
+        <BottomRecord />
+      </div>
     </div>
   );
 };
---
+++ front/components/recordFile/recordRight/RightPagi.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DashArrow } from "@/components/iconsvg";
+
+const RightPagiTop = () => {
+  return (
+    <div className="">
+      <div className=" flex justify-between mb-5">
+        <div>
+          <button className="btn bg-[#E5E7EB]">{"<"}</button>
+          <button className=" mx-3">Page 22</button>
+          <button className="btn bg-[#E5E7EB]">{">"}</button>
+        </div>
+        <div className="flex gap-10 rounded-md border-[1px] px-3 items-center bg-[#F9FAFB]">
+          <button className="bg-[#F9FAFB]"> Newest first</button>
+          <DashArrow />
+        </div>
+      </div>
+      <div className="bg-white px-4 py-2 flex rounded-xl justify-between border-[1px] items-center">
+        <div className="flex items-center gap-3">
+          <input type="checkbox" checked="checked" className="checkbox ml-3" />
+          <h2 className="font-medium">Select all</h2>
+        </div>
+        <h4 className="text-gray-500 font-semibold">- 35,000₮</h4>
+      </div>
+    </div>
+  );
+};
+
+export default RightPagiTop;
